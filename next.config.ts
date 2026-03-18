7e63193f42bc545d6112ecb1462c2edf518ec537
Nathan-14-05 <emmanuelnathan.mboueya@uphf.fr>
2026-03-18 12:39:58 +0100

    Update: next.config.ts pour export statique GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Indique à Next de générer des fichiers HTML statiques
+  images: {
+    unoptimized: true, // Obligatoire car GitHub Pages ne peut pas redimensionner les images à la volée
+  },
+  // Si ton dépôt s'appelle 'nathan-portfolio', décommente la ligne suivante :
+  // basePath: '/nathan-portfolio', 
 };
 
 export default nextConfig;
